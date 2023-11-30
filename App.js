6f3411d8267af1302d5f85ff3e4ce0949cb4a20d
Muhammad Husnain <150600466+Muhammad-Husnain007@users.noreply.github.com>
2023-11-30 09:38:18 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,101 +1,11 @@
 import React from 'react'
-import { Button, View } from 'react-native'
-// import ButtonComponent from './components/ButtonComponent'
-// import ToastComponent from './components/ToastComponent'
-// import AlertComponent from './components/AlertComponent'
-// import MyScrollView from './components/MyScrollView'
-// import MySwitch from './components/MySwitch'
-// import MyFlatList from './components/MyFlatList'
-import MySectionList from './components/MySectionList'
-// import MyImage from './components/MyImage'
+import { View, Text } from 'react-native'
+import Routing from './components/Routing'
 
 const App = () => {
   return (
-    // <View style={{ flex: 2 , justifyContent: "center" ,
-    // backgroundColor: 'orange'}}>
-    //    <Text style={{fontSize: 22}}>Navbar</Text>
-    //    <View style={{ flex: 4 , justifyContent: "center" ,
-    // alignItems: 'center',
-    // backgroundColor: 'red'}}>
-    //    <Text style={{fontSize: 22}}>Body</Text>
-    // </View>
-    // <View style={{ flex: 2 , justifyContent: "center" ,
-    // alignItems: 'center',
-    // backgroundColor: 'green'}}>
-    //    <Text style={{fontSize: 22}}>Footer</Text>
-
-    // </View>
-    // </View>
-    // <View style={{flex: 1, justifyContent: 'space-evenly',
-    // flexDirection: 'column',
-    // backgroundColor: 'aqua',
-    // alignItems: 'center'
-    // }}>
-    // <View style={{ width: 300, height: 50, borderRadius: 100,
-    //    paddingLeft: 100,
-    //    marginTop: 50,
-    //    paddingTop: 10,
-    //    backgroundColor: 'orange'}}>
-    //   <Text style={{fontSize: 18, }}>SignUp Form</Text>
-    // </View>
-    // <TextInput placeholder='Enter Your Name' 
-    // style={{backgroundColor: 'white',
-    // shadowColor: 'black',
-    // shadowRadius: 20,
-    // shadowOpacity: 10,
-    //  width: 300,
-    //  height: 50,
-    //  borderRadius: 100,
-    //  paddingLeft: 20,
-
-    // }}
-    //   />
-    //    <TextInput placeholder='Enter Your Email' 
-    // style={{backgroundColor: 'white',
-    // shadowColor: 'black',
-    // shadowRadius: 20,
-    // shadowOpacity: 10,
-    //  width: 300,
-    //  height: 50,
-    //  borderRadius: 100,
-    //  paddingLeft: 20,
-
-    // }}
-    //   />
-    //    <TextInput placeholder='Enter Your Password' 
-    // style={{backgroundColor: 'white',
-    // elevation: 5,
-    //  width: 300,
-    //  height: 50,
-    //  borderRadius: 100,
-    //  paddingLeft: 20,
-
-    // }}
-    //   />
-    // </View>
-
-  //   <View>
-  // <Button title='Submit' />
-  //   </View>
-
-  // <Buttons />
-  
-    // <Button color={'red'} title="On Press" />
-
-    // IMPORTS COMPONENTS
-
-  // <ButtonComponent />
-  // <ToastComponent />
-  // <AlertComponent />
-  // <MyScrollView />
-  // <MySwitch />
-  <View>
-  {/* <MyFlatList />  */}
-  {/* <MyImage /> */}
-   <MySectionList />
-</View>
+     <Routing />
   )
 }
 
 export default App
-
